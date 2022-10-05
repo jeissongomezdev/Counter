e41c4b6a10da1d65c6dea9a5add453f1466c2c09
Jeisson Gómez <jeissongomez@hotmail.es>
2022-10-04 20:25:26 -0500

    Agregando readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Counter
+
+[![Counter.png](https://i.postimg.cc/GmBq3jrg/Counter.png)](https://postimg.cc/hXB9pTXV)
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let count = 0;
 
-const value = document.querySelector("#value");
+const value = document.getElementById("value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
